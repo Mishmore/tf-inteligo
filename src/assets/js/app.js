02b7aa67af9c3d289954be1c5a6f2b82b862e626
Mishmore <michellemore28@gmail.com>
2017-07-19 20:53:15 -0500

    actualizando objetos

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,14 +1,21 @@
-var config = {
-  apiKey: "AIzaSyBau2K-1u9t_t9m9HTwk69rG0bTaS9lsWY",
-  authDomain: "prueba-7ce7e.firebaseapp.com",
-  databaseURL: "https://prueba-7ce7e.firebaseio.com",
-  projectId: "prueba-7ce7e",
-  storageBucket: "prueba-7ce7e.appspot.com",
-  messagingSenderId: "1027468621449"
-};
-firebase.initializeApp(config);
-var holaMundo = document.getElementById('holaMundo');
-var dbRef = firebase.database().ref().child('text');
-dbRef.on('value', snap => holaMundo.innerText = snap.val() );
 
-console.log('hola');
+$(_ => {
+  var config = {
+    apiKey: "AIzaSyBau2K-1u9t_t9m9HTwk69rG0bTaS9lsWY",
+    authDomain: "prueba-7ce7e.firebaseapp.com",
+    databaseURL: "https://prueba-7ce7e.firebaseio.com",
+    projectId: "prueba-7ce7e",
+    storageBucket: "prueba-7ce7e.appspot.com",
+    messagingSenderId: "1027468621449"
+  };
+  firebase.initializeApp(config);
+  //var holaMundo = document.getElementById('holaMundo');
+  //var dbRef = firebase.database().ref().child('text');
+  //dbRef.on('value', snap => holaMundo.innerText = snap.val() );
+
+  var objetoPre = $('#objeto');
+  var dbRefObjeto = firebase.database().ref().child('objeto');
+  dbRefObjeto.on('value', snap => console.log(snap.val()));
+
+
+})
